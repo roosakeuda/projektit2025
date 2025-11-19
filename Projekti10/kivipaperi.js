6af256e466fef774eb298099bac7b4fec21f9e5c
roosakeuda <roosa.vaaranen@edu.keuda.fi>
2025-11-19 08:39:16 +0200

    Projektit

--- Projekti10/kivipaperi.js
+++ Projekti10/kivipaperi.js
@@ -1,10 +1,10 @@
 let lista = ["kivi", "sakset", "paperi"]
-let kauttaja = 0
-let tietokone = 0
+let kauttaja = 0 // laskee käyttäjän voitot
+let tietokone = 0 // laskee tietokoneen voitot
 let koneenValinta = ""
 let indexi = 0
 let vastaus = ""
-let voittoHavio = 0
+let voittoHavio = 0 // ilmoittaa onko viimeinen peli voitettu vai hävitty. 1 = voitto, -1 = häviö
 
 
 function peli(tulos){ //kutsu tätä funktiota html elementtiä klikatessa, esim näin: onclick="peli('kivi')"
@@ -42,23 +42,33 @@ function peli(tulos){ //kutsu tätä funktiota html elementtiä klikatessa, esim
 
 
     document.getElementById("tulos").innerHTML = 
-        vastaus // Näyttää html tiedostossa tulostekstin
+        vastaus // Näyttää html tiedostossa voitto/häviötekstin kohtaan: id="tulos"
     document.getElementById("voitot").innerHTML = 
-        `Käyttäjä: ${kauttaja} / Tietokone: ${tietokone}` //
-    document.getElementById("kayttaja").textContent = 
-        kauttaja
-    document.getElementById("tietokone").textContent = 
-        tietokone
+        `Käyttäjä: ${kauttaja} / Tietokone: ${tietokone}` //näyttää tulospisteen tekstin id="voitot" kohtaan
+        
+    // document.getElementById("kayttaja").textContent = 
+        // kauttaja 
+    // document.getElementById("tietokone").textContent = 
+        // tietokone
+
+    // tuossa erikseen kaksi lausetta, jotka laittavat id="kayttaja" sekä id="tietokone" kohtaan erikseen joko käyttäjän pisteet, tai koneen pisteet. Voi käyttää jos haluaa, esim jos tulosboksin tekeminen on niillä helpompi
 
     lista.forEach(valinta =>{
         let nappi = document.getElementById(valinta)
         nappi.classList.remove("oikein", "vaarin")
-        })
-    let nappiPelaaja = document.getElementById(valinta)
-    if (voittoHavio === 1){
-        nappiPelaaja.classList.add("oikein")
-    }else if (voittoHavio === -1){
-        nappiPelaaja.classList.add("vaarin")
-    }
+    }); // otetaan valinnalta pois "oikein", ja "vaarin" css luokat
 
+    let nappiPelaaja = document.getElementById(tulos)
+    if (voittoHavio === 1){
+        nappiPelaaja.classList.add("oikein") // Jos käyttäjä voitti, lisätään css luokka "oikein"
+
+        setTimeout(() => {
+            nappiPelaaja.classList.remove("oikein")
+        }, 1500) // ajastetaan se lähtemään 1,5s kuluttua
+    }else if (voittoHavio === -1){
+        nappiPelaaja.classList.add("vaarin") //Jos käyttäjä hävisi, lisätään css luokka "vaarin"
+        setTimeout(() => {
+            nappiPelaaja.classList.remove("vaarin")
+        }, 1500)
+    }
 }
